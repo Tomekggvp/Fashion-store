188d1f92c88f321a14e806619d1198f1f4dfa5ed
Tomekggvp <tomek2005@mail.ru>
2025-04-13 00:12:35 +0300

    Arrivals and brand section

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,7 @@
 import Header from "./components/header/Header";
 import Promo from "./components/promo/Promo";
+import Brands from "./components/brands/Brands";
+import Arrivals from "./components/arrivals/Arrivals";
 
 function App() {
 
@@ -8,7 +10,11 @@ function App() {
      
     <Header />
 
-    <Promo/>
+    <Promo />
+
+    <Brands />
+
+    <Arrivals />
 
     </div>
   );
